826996f132f52c2d02952d25882d69bcb13e0c6e
kajuettner <105049558+kajuettner@users.noreply.github.com>
2024-05-03 00:56:03 +0200

    add saal-view

---
+++ Cinemagic/cinemagic/src/app/saal-view/saal-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saal-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './saal-view.component.html',
+  styleUrl: './saal-view.component.css'
+})
+export class SaalViewComponent {
+
+}
