e3b002f3f29d55c8317caf90a14ccac0a6333de5
kajuettner <105049558+kajuettner@users.noreply.github.com>
2024-05-03 01:06:28 +0200

    Update app.component.spec.ts
    Change Title

--- Cinemagic/cinemagic/src/app/app.component.spec.ts
+++ Cinemagic/cinemagic/src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'cinemagic' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cinemagic');
+    expect(app.title).toEqual('Cinemagic');
   });
 
   it('should render title', () => {
