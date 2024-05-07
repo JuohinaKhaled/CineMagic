aa55540d0d6e4926fe552140941fafc2667dd955
Juohina Khaled <Jkhaled@stud.hs-bremen.de>
2024-05-07 21:46:20 +0200

    neue Struktur

--- Cinemagic/cinemagic/src/app/app.routes.ts
+++ Cinemagic/cinemagic/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Route } from '@angular/router';
-import { LoginViewComponent } from './login-view/login-view.component';
-import { SignUpViewComponent } from './sign-up-view/sign-up-view.component';
+import { LoginViewComponent } from './views/login-view/login-view.component';
+import { SignUpViewComponent } from './views/sign-up-view/sign-up-view.component';
 
 export const routes: Route[] = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
