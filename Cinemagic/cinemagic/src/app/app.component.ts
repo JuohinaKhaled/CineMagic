1ff764855db10886e6dc0888f134bf7660d75717
Juohina Khaled <Jkhaled@stud.hs-bremen.de>
2024-05-13 18:11:14 +0200

    Login in header verlinkt

--- Cinemagic/cinemagic/src/app/app.component.ts
+++ Cinemagic/cinemagic/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cinemagic';
+  constructor(private router: Router) {
+    router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.isLoginRoute();
+    });
+  }
+
+  isLoginRoute() {
+    return this.router.url === '/login';
+  }
 }
---
+++ Cinemagic/cinemagic/src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+// login.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  user = {
+    email: '',
+    password: ''
+  };
+
+  onSubmit() {
+    console.log(this.user); // Here you would handle the login logic
+  }
+}
