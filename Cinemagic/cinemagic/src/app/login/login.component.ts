ae098d52f25f972819a0d75e6513224bf36982d0
Juohina Khaled <Jkhaled@stud.hs-bremen.de>
2024-06-02 15:01:13 +0200

    extra

--- Cinemagic/cinemagic/src/app/login/login.component.ts
+++ Cinemagic/cinemagic/src/app/login/login.component.ts
@@ -1,7 +1,8 @@
-// login.component.ts
 import { Component } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CustomSnackbarComponent } from '../custom-snackbar/custom-snackbar.component';
 
 @Component({
   selector: 'app-login',
@@ -16,23 +17,40 @@ export class LoginComponent {
 
   loginFailed = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   onSubmit() {
     this.authService.login(this.user.email, this.user.password).subscribe(
       response => {
         if (response.status === 'success') {
           console.log('Login successful');
+          this.openSnackBar('Login successful!');
           this.router.navigate(['/home']);
         } else {
           console.log('Login failed');
+          this.openSnackBar('Login failed. Please check your credentials.');
           this.loginFailed = true;
         }
       },
       error => {
         console.log('Error during login:', error);
+        this.openSnackBar('An error occurred during login. Please try again.');
         this.loginFailed = true;
       }
     );
+  }
+
+  openSnackBar(message: string) {
+    this.snackBar.openFromComponent(CustomSnackbarComponent, {
+      data: { message: message },
+      duration: 5000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      panelClass: 'custom-snackbar'
+    });
   }
 }
