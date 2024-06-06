65531a9c9fefd1900ec419da0a946973234c8967
Juohina Khaled <Jkhaled@stud.hs-bremen.de>
2024-06-06 21:14:40 +0200

    Detail Seite

--- Cinemagic/cinemagic/src/app/movie/movie-details/movie-details.component.ts
+++ Cinemagic/cinemagic/src/app/movie/movie-details/movie-details.component.ts
@@ -1,63 +1,94 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {MovieService} from "../movie.service";
-import {EventService} from "../../event/service/event.service";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MovieService } from '../movie.service';
+import { EventService } from '../../event/service/event.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
-  styleUrl: './movie-details.component.css'
+  styleUrls: ['./movie-details.component.css']
 })
 export class MovieDetailsComponent implements OnInit {
-  movieID: number = 0;
-  movie: any;
-  events: any [] = [];
+  filmID: number = 0;
+  film: any = {};
+  events: any[] = [];
+  groupedEvents: any[] = [];
+  displayLimit: number = 3;
 
-  constructor(private movieService : MovieService, private eventService : EventService,
-              private route: ActivatedRoute) {
-  }
+  constructor(
+    private movieService: MovieService,
+    private eventService: EventService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.getMovieID();
-    this.getMovie();
+    this.getFilmID();
+    this.getFilm();
     this.getEvents();
   }
 
+  getFilmID() {
+    this.filmID = +this.route.snapshot.paramMap.get('movieID')!;
+    console.log('Film ID:', this.filmID);
 
-  getMovieID() {
-    this.movieID = +this.route.snapshot.paramMap.get('movieID')!;
-    console.log('Movie ID:', this.movieID);
-
-    if (!this.movieID) {
-      console.error('No movie data found in state. Redirecting to movie list.');
+    if (!this.filmID) {
+      console.error('No film data found in state. Redirecting to film list.');
     } else {
-      console.log('Movie data loaded:', this.movieID);
+      console.log('Film data loaded:', this.filmID);
     }
-
   }
 
-  getMovie() {
-    this.movieService.getMovieDetails(this.movieID).subscribe(
-      (data ) => {
-        this.movie = data;
-        console.log('Movie loaded:', this.movie);
+  getFilm() {
+    this.movieService.getMovieDetails(this.filmID).subscribe(
+      (data) => {
+        if (data && data.length > 0) {
+          this.film = data[0];
+          console.log('Film loaded:', this.film);
+        } else {
+          console.error('No film data found');
+        }
       },
       (error) => {
-        console.error('Error fetching movie details:', error);
+        console.error('Error fetching film details:', error);
       }
     );
   }
 
-  getEvents(){
-    this.eventService.getEvents(this.movieID).subscribe(
+  getEvents() {
+    this.eventService.getEvents(this.filmID).subscribe(
       (data) => {
-        this.events = data; // assuming the response is an array with a single movie object
-        console.log('Events:', this.events);
+        this.events = data;
+        this.groupEventsByDate();
+        console.log('Events loaded:', this.events);
       },
       (error) => {
         console.error('Error fetching events:', error);
       }
     );
+  }
 
+  groupEventsByDate() {
+    const grouped = _.groupBy(this.events, 'Vorfuehrungsdatum');
+    this.groupedEvents = Object.keys(grouped).map(date => ({
+      date,
+      events: grouped[date],
+      eventsToShow: grouped[date].slice(0, this.displayLimit)
+    }));
+  }
+
+  goToRoom(eventID: number) {
+    this.router.navigate(['/room', eventID, this.filmID]);
+  }
+
+  showMore(date: string) {
+    this.groupedEvents = this.groupedEvents.map(group => {
+      if (group.date === date) {
+        const newLimit = group.eventsToShow.length + this.displayLimit;
+        return { ...group, eventsToShow: group.events.slice(0, newLimit) };
+      }
+      return group;
+    });
   }
 }
